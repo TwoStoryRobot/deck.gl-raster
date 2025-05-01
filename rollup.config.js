da3740018cf82e629e1fdc4e16da3eeed33a9113
Caleb Sharp <me@calebissharp.com>
2025-05-01 15:08:56 -0700

    chore: add typescript build configuration

--- rollup.config.js
+++ rollup.config.js
@@ -1,33 +1,46 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import {terser} from 'rollup-plugin-terser';
+import typescript from '@rollup/plugin-typescript';
+import terser from '@rollup/plugin-terser';
 import glsl from 'rollup-plugin-glsl';
+import dts from 'rollup-plugin-dts';
 
-const config = (file, plugins = []) => ({
-  input: 'src/index.js',
-  output: {
-    name: 'deck.gl-raster',
-    format: 'umd',
-    indent: false,
-    file,
-    globals: {
-      '@deck.gl/core': 'deck',
-      '@deck.gl/layers': 'deck',
-      '@deck.gl/mesh-layers': 'deck',
-      '@luma.gl/core': 'luma',
-      '@luma.gl/constants': 'luma.GL',
-      '@luma.gl/engine': 'luma',
-      '@luma.gl/webgl': 'luma',
+const config = {
+  input: 'src/index.ts',
+  output: [
+    {
+      name: 'deck.gl-raster',
+      format: 'es',
+      indent: false,
+      file: './dist/deck.gl-raster.mjs',
+      sourcemap: true,
     },
-  },
+    {
+      name: 'deck.gl-raster',
+      format: 'umd',
+      indent: false,
+      file: './dist/deck.gl-raster.umd.js',
+      sourcemap: true,
+      globals: {
+        '@deck.gl/core': 'deck',
+        '@deck.gl/layers': 'deck',
+        '@deck.gl/mesh-layers': 'deck',
+        '@luma.gl/core': 'luma',
+        '@luma.gl/constants': 'luma.GL',
+        '@luma.gl/engine': 'luma',
+        '@luma.gl/shadertools': 'luma',
+        '@luma.gl/webgl': 'luma',
+      },
+    },
+  ],
   plugins: [
-    resolve(),
+    typescript(),
     commonjs(),
-    ...plugins,
+    resolve(),
     glsl({
       include: 'src/**/*.glsl',
       sourceMap: true,
-      compress: true
+      compress: true,
     }),
   ],
   external: [
@@ -37,11 +50,16 @@ const config = (file, plugins = []) => ({
     '@luma.gl/core',
     '@luma.gl/constants',
     '@luma.gl/engine',
+    '@luma.gl/shadertools',
     '@luma.gl/webgl',
   ],
-});
+};
 
 export default [
-  config('dist/dist.js'),
-  config('dist/dist.min.js', [terser()]),
+  config,
+  {
+    input: './src/index.ts',
+    output: [{file: 'dist/deck.gl-raster.d.ts', format: 'es', sourcemap: true}],
+    plugins: [dts()],
+  },
 ];
